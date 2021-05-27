e24064b21eecfc3912d1c4874e0a5a84bac90aee
Your Name <you@example.com>
2021-05-27 22:34:42 +0000

    added data file

--- subnets.tf
+++ subnets.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "private-subnet1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-private-subnets[0] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[0]
 
   tags = {
     Name = var.private-subnet-names[0] #This goes to module
@@ -13,6 +14,7 @@ resource "aws_subnet" "private-subnet2" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-private-subnets[1] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[1]
 
   tags = {
     Name = var.private-subnet-names[1] #This goes to module
@@ -23,6 +25,7 @@ resource "aws_subnet" "private-subnet3" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-private-subnets[2] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[2]
 
   tags = {
     Name = var.private-subnet-names[2] #This goes to module
@@ -35,6 +38,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-public-subnets[0] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[0]
 
   tags = {
     Name = var.public-subnet-names[0] #This goes to module
@@ -45,6 +49,7 @@ resource "aws_subnet" "public-subnet2" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-public-subnets[1] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[1]
 
   tags = {
     Name = var.public-subnet-names[1] #This goes to module
@@ -55,6 +60,7 @@ resource "aws_subnet" "public-subnet3" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = var.cidr-block-public-subnets[2] #This goes to module
   map_public_ip_on_launch = true
+  availability_zone_id = data.aws_availability_zones.name.id[2]
 
   tags = {
     Name = var.public-subnet-names[2] #This goes to module
